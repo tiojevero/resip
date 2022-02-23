2abb27f38369ef10aac4c2975eca06adc1b71a78
tiojevero <tiojevero@gmail.com>
2022-02-23 23:03:36 +0700

    add empty state when search value is not found

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -7,27 +7,32 @@ defineProps({
 </script>
 
 <template>
-    <div
-        class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5"
-        v-if="data"
-    >
-        <router-link
-            class="p-3 rounded-2xl shadow-md hover:shadow-lg"
-            v-for="meal in data.meals"
-            :key="meal.idMeal"
-            :to="`/recipe/${meal.idMeal}`"
+    <div v-if="data">
+        <div
+            class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5"
+            v-if="data.meals"
         >
-            <div class="aspect-square bg-gray-100 rounded-lg overflow-hidden">
-                <img
-                    :src="meal.strMealThumb"
-                    :alt="meal.strMealThumb"
-                    loading="lazy"
-                />
-            </div>
-            <h5 class="font-semibold line-clamp-2 mt-2 mb-4">
-                {{ meal.strMeal }}
-            </h5>
-        </router-link>
+            <router-link
+                class="p-3 rounded-2xl shadow-lg"
+                v-for="meal in data.meals"
+                :key="meal.idMeal"
+                :to="`/recipe/${meal.idMeal}`"
+            >
+                <div
+                    class="aspect-square bg-gray-100 rounded-lg overflow-hidden"
+                >
+                    <img
+                        :src="meal.strMealThumb"
+                        :alt="meal.strMealThumb"
+                        loading="lazy"
+                    />
+                </div>
+                <h5 class="font-semibold line-clamp-2 mt-2 mb-4">
+                    {{ meal.strMeal }}
+                </h5>
+            </router-link>
+        </div>
+        <p v-else>Sorry, i can't find your favorite foods ☹</p>
     </div>
     <p v-else>Loading...</p>
 </template>
